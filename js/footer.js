11e22b79a0d054ec85519eae6fe7c25caf5a7674
korze <korze84@gmail.com>
2025-03-22 20:56:30 -0700

    Redesign footer as a distro card at the end of the list

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,9 @@
 fetch('elements/footer.html')
     .then(response => response.text())
     .then(data => {
-        document.getElementById('footer').innerHTML = data;
+        // Add the footer card as the last card in the OS sections
+        document.querySelector('.os-sections').insertAdjacentHTML('beforeend', data);
+        
+        // Hide the original footer div since we don't need it anymore
+        document.getElementById('footer').style.display = 'none';
     });
